b58c05e9b2d7ecf187cb9f05a524ccf0137dc095
Mauricio Barros <mauriciobajesus@gmail.com>
2022-02-23 21:12:34 -0300

    Add user_processing

--- user_processing.py
+++ user_processing.py
@@ -1,6 +1,9 @@
 from airflow.models import DAG
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+import json
+
 from datetime import datetime
 
 default_args = {
@@ -8,7 +11,7 @@ default_args = {
 }
 
 with DAG(
-          'user_processing'
+        'user_processing'
         , schedule_interval='@daily'
         , default_args=default_args
         , catchup=False) as dag:
@@ -32,5 +35,15 @@ with DAG(
         task_id='is_api_available'
         , http_conn_id='user_api_test_airflow'
         , endpoint='api/'
+
+    )
+
+    extracting_user = SimpleHttpOperator(
+        task_id='extracting_user'
+        , http_conn_id='user_api_test_airflow'
+        , endpoint='api/'
+        , method='GET'
+        , response_filter=lambda response: json.loads(respose.text)
+        , log_response=True
 
     )
